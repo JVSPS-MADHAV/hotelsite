96fb198bed30e7ce55d2a2771d63b719f9ad85d6
JUJHALRAO VENKATA SATYA PAVAN SAI MADHAV <112550698+JVSPS-MADHAV@users.noreply.github.com>
2023-07-29 20:04:27 +0530

    Add files via upload

---
+++ src/components/AirbnbHome.js
@@ -0,0 +1,88 @@
+import  './AirbnbHome.css';
+import BookPage from './Book';
+import { useState } from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDGyzuyIWtwvYMDRRo-MugzB0RizaMGaD0",
+    authDomain: "hotel-22d8b.firebaseapp.com",
+    projectId: "hotel-22d8b",
+    storageBucket: "hotel-22d8b.appspot.com",
+    messagingSenderId: "629858363468",
+    appId: "1:629858363468:web:c05aa32fa48dc1cb8e6ce9"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export const db=getFirestore(app);
+
+const Home = () => {
+    const [valid, setvalid] = useState(true)
+    const [hide, sethide] = useState(true)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const book_date = document.getElementById('book-date');
+        const book_time = document.getElementById('book-time');
+        
+        const leave_date = document.getElementById('leave-date');
+        const leave_time = document.getElementById('leave-time');
+
+        if(!book_date.value || !book_time.value || !leave_date.value || !leave_time.value){
+            alert('Please fill all the fields');
+        }else{
+            writeUserData();
+        function writeUserData() {
+            const db = getDatabase();
+            set(ref(db, 'data/'), {
+            bookingdate: book_date.value,
+            bookingtime: book_time.value,
+            leave_date: leave_date.value,
+            leave_time: leave_time.value,
+            });
+        }
+            alert('Your booking has been made');
+            const BookPageDiv = document.querySelector('.box');
+            const container = document.querySelector('.container');
+            setvalid(!valid + BookPageDiv.classList.add("show"));
+            sethide(!hide + container.classList.add("hide"));
+        }
+    }
+  return (
+      <>
+        <BookPage />
+        <div className="container">
+      <div className="content">
+        <div className="text">Book Now</div>
+        <div className="form2">
+          <div className="txt">Date & Time you would like to Stay</div>
+          <form action="" onSubmit={handleSubmit}>
+            <div className="inputData">
+              <input type="date" name="" id="book-date"/>
+            </div>
+            <div className="inputData">
+              <input type="time" name="" id="book-time" />
+            </div>
+            <div className="txt">Date & Time you would like to leave</div>
+            <div className="inputData">
+              <input type="date" name="" id="leave-date" />
+            </div>
+            <div className="inputData">
+              <input type="time" name="" id="leave-time" />
+            </div>
+            <div className="book">
+                <button type="submit">Book</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+      </>
+  );
+};
+export default Home;
